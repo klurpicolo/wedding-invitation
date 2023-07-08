715df1cd8017956d4637150f5e7aca3be4620cd4
waritboonmasiri <warit.boonmasiri@gmail.com>
2023-07-08 14:21:51 +0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import logo from '$lib/assets/signature.svg';
+    import bg1 from '$lib/assets/hand-painted-watercolor-pastel-sky-background_23-2148902771.avif'
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<img alt="The project logo" src={logo} />
+
+
+<div class="container min-h-screen bg-gradient-to-t">
+    <p class="text-2xl">Save The Date</p>
+    <p>November 23, 2023</p>
+</div>
