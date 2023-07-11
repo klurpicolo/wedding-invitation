a9547d37a95882c6c96a901def1b5833d0973d30
waritboonmasiri <warit.boonmasiri@gmail.com>
2023-07-11 22:28:02 +0700

    Add Gallery

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <script>
 	import InviCard from './InvitationCard.svelte';
+	import Gallery from './Gallery.svelte';
 </script>
 
 <div>
-	<InviCard></InviCard>
+	<InviCard />
+</div>
+
+<div>
+	<Gallery></Gallery>
 </div>
