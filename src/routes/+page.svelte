7293b7e2874d683f5ebb64f5db42e318fa20f483
waritboonmasiri <warit.boonmasiri@gmail.com>
2023-09-01 23:05:40 +0700

    test dvh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,9 +28,9 @@
 			<div class="h-2/3 w-full left-top ">
 				<img class="object-contain h-full w-full" src={weddingLogo} alt="" />
 			</div>
-			<div class="h-1/3 right-buttom">
+			<div class="h-1/3 right-buttom mb-16">
 				<div
-					class="h-full text-center flex flex-col justify-center items-center font-montserrat font-light motion-safe:animate-fadeIn text-black mb-8"
+					class="h-full text-center flex flex-col justify-center items-center font-montserrat font-light motion-safe:animate-fadeIn text-black"
 				>
 					<div class="text-3xl">Join us to celebate the wedding of</div>
 					<div class="text-6xl">Pattaraporn</div>
@@ -50,12 +50,12 @@
 				</div>
 				<div class="h-1/2 w-1/2 ml-auto mr-8">
 					<div class=""> 
-						<img class="" src={klurBio} />
+						<img class="stroke-orange-50 stroke-2" src={klurBio} />
 						<p class="text-xl text-black text-center">Warit Boonmasiri(Klur)</p>
 					</div>
 				</div>
 			</div>
-			<div class="right-buttom h-2/5 m-8">
+			<div class="right-buttom h-2/5 m-8 mb-16">
 				<div class="m-auto text-black text-xl text-center">
 					<img class="h-1/2 w-4/5 m-auto mt-8 mx-auto font-montserrat rounded-xl" src={couple1} />
 					<p>Save the date</p>
@@ -65,7 +65,7 @@
 		</div>
 
 		<div class="scroll-area">
-			<div class="m-8">
+			<div class="h-full mx-4 md:mx-32 flex justify-center items-center">
 				<Gal />
 			</div>
 		</div>
@@ -74,7 +74,7 @@
 			<div class="h-1/6 w-fulltext-center">
 				<p class="my-auto text-black">Timeline</p>
 			</div>
-			<div class="flex flex-row h-5/6 w-full max-w-3xl">
+			<div class="flex flex-row h-5/6 w-full max-w-3xl mb-16">
 				<div class="w-1/2 my-auto">
 					<TimeLineItem image={item1} time="07.09" desc="พิธีตักบาตรเช้า"></TimeLineItem>
 					<TimeLineItem image={item2} time="08.09" desc="พิธีแห่ขันหมาก"></TimeLineItem>
@@ -99,7 +99,10 @@
 					<p>อำเภอเมือง จังหวัดราชบุรี</p>
 				</div>
 				<div class="h-3/4 md:h-1/2 w-4/5 mx-auto shadow-xl">
-					<Map />
+						<Map />
+						<div class="w-24 bg-slate-400 text-base px-8 py-2 my-8 rounded-lg shadow-lg hover:scale-125 transition-all delay-100">
+							<a class="h-full w-full m-auto" target="_blank" href="https://www.google.com/maps/place/155-Camp+%E0%B8%AB%E0%B8%99%E0%B8%AD%E0%B8%87%E0%B8%81%E0%B8%A5%E0%B8%B2%E0%B8%87%E0%B8%99%E0%B8%B2+155+%E0%B8%95%E0%B8%B3%E0%B8%9A%E0%B8%A5+%E0%B8%AB%E0%B8%99%E0%B8%AD%E0%B8%87%E0%B8%81%E0%B8%A5%E0%B8%B2%E0%B8%87%E0%B8%99%E0%B8%B2+%E0%B9%80%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B8%87+%E0%B8%A3%E0%B8%B2%E0%B8%8A%E0%B8%9A%E0%B8%B8%E0%B8%A3%E0%B8%B5+70000/@13.6099959,99.7975365,17z/data=!4m6!3m5!1s0x30e3250d8f1e5909:0x54d6a4e0440e274e!8m2!3d13.6099959!4d99.7975365!16s%2Fg%2F11pb8c7ggm">Direction</a>
+						</div>
 				</div>
 			</div>
 			<div class="right-buttom text-black text-center m-auto w-screen md:w-3/4">
@@ -110,7 +113,7 @@
 				</div>
 
 				<p>Remark</p>
-				<p>Thanks</p>
+				<p class="text-lg">Thank you for being an important part of our story</p>
 			</div>
 		</div>
 	</div>
@@ -123,7 +126,7 @@
 	.scroll-container,
 	.scroll-area {
 		/* width: 100vw; */
-		height: 100vh;
+		height: 100dvh;
 		font-size: 60px;
 		/* w-full max-w-xl */
 	}
