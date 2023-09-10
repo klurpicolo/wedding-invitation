e787a38b1f860fbb7e117c37476ff3ee16cbc660
waritboonmasiri <warit.boonmasiri@gmail.com>
2023-09-11 00:23:50 +0700

    Format Gal2

--- src/lib/Gal2.svelte
+++ src/lib/Gal2.svelte
@@ -1,48 +1,53 @@
 <script>
-  import couple15 from '$lib/assets/couple/pic15.jpg';
-  import couple16 from '$lib/assets/couple/pic16.jpg';
-  import couple17 from '$lib/assets/couple/pic17.jpg';
-  import couple18 from '$lib/assets/couple/pic18.png';
-  import couple20 from '$lib/assets/couple/pic20.png';
-  import couple21 from '$lib/assets/couple/pic21.jpg';
-  import couple22 from '$lib/assets/couple/pic22.jpg';
-  import couple23 from '$lib/assets/couple/pic23_2.jpg';
+	import couple15 from '$lib/assets/couple/pic15.jpg';
+	import couple16 from '$lib/assets/couple/pic16.jpg';
+	import couple17 from '$lib/assets/couple/pic17.jpg';
+	import couple18 from '$lib/assets/couple/pic18.png';
+	import couple20 from '$lib/assets/couple/pic20.png';
+	import couple21 from '$lib/assets/couple/pic21.jpg';
+	import couple22 from '$lib/assets/couple/pic22.jpg';
+	import couple23 from '$lib/assets/couple/pic23_2.jpg';
 </script>
 
 <div class="relative flex flex-wrap justify-center">
-  <div class="relative flex-auto w-1/2 md:flex-1 max-w-[40%] md:max-w-full">
-    <img class="h-1/3 w-full object-cover" src={couple23} alt="" />
-    <img class="h-2/3 w-full object-cover" src={couple21} alt="" />
-  </div>
-  <div class="relative flex-auto w-1/2 md:flex-1 max-w-[40%]">
-    <img class="h-2/3 w-full object-cover" src={couple22} alt="" />
-    <img class="h-1/3 w-full object-cover" src={couple18} alt="" />
-  </div>
-  <div class="relative flex-auto w-1/2 md:flex-1 max-w-[40%]">
-    <img class="h-1/3 w-full object-cover"  src={couple15} alt="" />
-    <div class="relative h-2/3 w-full">
-      <div class="h-fit w-fit bg-[#AAA492] shadow-2xl absolute -top-4 -right-2 text-base md:text-4xl flex flex-col text-[#FBFBFB] p-2 z-20 font-dancing-script">
-        <p class="pl">Grow</p>
-        <p class="pl-6">old</p>
-        <p class="pl-12">together</p>
-      </div>
-      <img class="h-full w-full object-cover" src={couple16} alt="" />
-    </div>
-    
-    
-  </div>
-  <div class="relative flex-auto w-1/2 md:flex-1 max-w-[40%]">
-    <img class="h-2/3 w-full object-cover" src={couple17} alt="" />
-    <img class="h-1/3 w-full object-cover" src={couple20} alt="" />
-  </div>
-  <div class="h-fit w-fit max-w-[8rem] md:max-w-[24rem] bg-[#AAA492] shadow-2xl absolute top-2 right-2 text-left text-base md:text-2xl text-[#FBFBFB] mr-4 p-2 font-dancing-script">
-    My love, you are worth it all
-  </div>
-  <div class="h-fit w-[15%] bg-[#819171] shadow-2xl absolute invisible md:visible top-24 left-1/3 text-base text-[#FBFBFB] mr-4 p-2 font-dancing-script">
-    I love you not for who you are, but who I am when I'm by your side.
-  </div>
-  <div class="h-fit w-[15%] bg-[#819171] shadow-2xl absolute bottom-4 left-4 invisible md:visible text-xl text-[#FBFBFB] mr-4 p-2 font-dancing-script">
-    Together with you is my favorite place to be
-  </div>
-  
+	<div class="relative flex-auto w-1/2 md:flex-1 max-w-[40%] md:max-w-full">
+		<img class="h-1/3 w-full object-cover" src={couple23} alt="" />
+		<img class="h-2/3 w-full object-cover" src={couple21} alt="" />
+	</div>
+	<div class="relative flex-auto w-1/2 md:flex-1 max-w-[40%]">
+		<img class="h-2/3 w-full object-cover" src={couple22} alt="" />
+		<img class="h-1/3 w-full object-cover" src={couple18} alt="" />
+	</div>
+	<div class="relative flex-auto w-1/2 md:flex-1 max-w-[40%]">
+		<img class="h-1/3 w-full object-cover" src={couple15} alt="" />
+		<div class="relative h-2/3 w-full">
+			<div
+				class="h-fit w-fit bg-[#AAA492] shadow-2xl absolute -top-4 -right-2 text-base md:text-4xl flex flex-col text-[#FBFBFB] p-2 z-20 font-dancing-script"
+			>
+				<p class="pl">Grow</p>
+				<p class="pl-6">old</p>
+				<p class="pl-12">together</p>
+			</div>
+			<img class="h-full w-full object-cover" src={couple16} alt="" />
+		</div>
+	</div>
+	<div class="relative flex-auto w-1/2 md:flex-1 max-w-[40%]">
+		<img class="h-2/3 w-full object-cover" src={couple17} alt="" />
+		<img class="h-1/3 w-full object-cover" src={couple20} alt="" />
+	</div>
+	<div
+		class="h-fit w-fit max-w-[8rem] md:max-w-[24rem] bg-[#AAA492] shadow-2xl absolute top-2 right-2 text-left text-base md:text-2xl text-[#FBFBFB] mr-4 p-2 font-dancing-script"
+	>
+		My love, you are worth it all
+	</div>
+	<div
+		class="h-fit w-[15%] bg-[#819171] shadow-2xl absolute invisible md:visible top-24 left-1/3 text-base text-[#FBFBFB] mr-4 p-2 font-dancing-script"
+	>
+		I love you not for who you are, but who I am when I'm by your side.
+	</div>
+	<div
+		class="h-fit w-[15%] bg-[#819171] shadow-2xl absolute bottom-4 left-4 invisible md:visible text-xl text-[#FBFBFB] mr-4 p-2 font-dancing-script"
+	>
+		Together with you is my favorite place to be
+	</div>
 </div>
