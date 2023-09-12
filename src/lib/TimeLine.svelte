fb38772289571d5efdc18e05c767ea202bc6defa
waritboonmasiri <warit.boonmasiri@gmail.com>
2023-09-12 23:16:17 +0700

    Extract timeline

---
+++ src/lib/TimeLine.svelte
@@ -0,0 +1,55 @@
+<script>
+    import TimeLineItem from '$lib/TimeLineItem.svelte';
+
+    import item1 from '$lib/assets/timeline/1.png';
+	import item2 from '$lib/assets/timeline/2.png';
+	import item3 from '$lib/assets/timeline/3.png';
+	import item4 from '$lib/assets/timeline/4.png';
+</script>
+
+
+<div class="flex flex-row h-5/6 w-full max-w-3xl mb-10">
+    <div class="w-1/2 mb-auto ml-auto">
+        <div class="flex relative mb-4">
+            <TimeLineItem
+                image={item1}
+                time="07.09"
+                desc="พิธีตักบาตรเช้า"
+                additionalClass="mx-auto"
+            />
+            <div class="h-4 w-4 absolute -right-[10px] top-1/2 rounded-full bg-slate-600" />
+        </div>
+        <div class="flex relative">
+            <TimeLineItem
+                image={item3}
+                time="09.39"
+                desc="พิธีหลั่งน้ำสังข์"
+                additionalClass="mx-auto"
+            />
+            <div class="h-4 w-4 absolute -right-[10px] top-1/2 rounded-full bg-slate-600" />
+        </div>
+    </div>
+    <div class="h-5/6 w-fit my-auto py-4 flex flex-col items-center">
+        <div class="w-1 h-full bg-black" />
+    </div>
+    <div class="w-1/2 my-auto pt-24 mr-auto">
+        <div class="relative">
+            <TimeLineItem
+                image={item2}
+                time="08.09"
+                desc="พิธีแห่ขันหมาก"
+                additionalClass="mx-auto"
+            />
+            <div class="h-4 w-4 absolute -left-[10px] top-1/2 rounded-full bg-slate-600" />
+        </div>
+        <div class="relative">
+            <TimeLineItem
+                image={item4}
+                time="11.00"
+                desc="ร่วมรับประทานอาหาร"
+                additionalClass="mx-auto"
+            />
+            <div class="h-4 w-4 absolute -left-[10px] top-1/2 rounded-full bg-slate-600" />
+        </div>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,30 +2,29 @@
 	import weddingLogo from '$lib/assets/signature2.svg';
 	import bg from '$lib/assets/bg1_com.png';
 
-	import KlurAndDao from '$lib/KlurAndDao.svelte'
+	import KlurAndDao from '$lib/KlurAndDao.svelte';
 	import bg4 from '$lib/assets/bg4_com.png';
 	import bg5 from '$lib/assets/bg5_com.jpeg';
 
 	import Gal from '$lib/Gal2.svelte';
 
+	import TimeLine from '$lib/TimeLine.svelte';
+
 	import locationLogo from '$lib/assets/location/location-sign-svgrepo-com.svg';
 	import Map from '$lib/Map.svelte';
-
-	import item1 from '$lib/assets/timeline/1.png'
-	import item2 from '$lib/assets/timeline/2.png'
-	import item3 from '$lib/assets/timeline/3.png'
-	import item4 from '$lib/assets/timeline/4.png'
-
 	import CountDown from '$lib/CountDown.svelte';
-	import TimeLineItem from '$lib/TimeLineItem.svelte';
 </script>
 
 <div class="support-scrollsnap" />
 
 <div class="scroll-container">
-	
-	<div class="scroll-area flex-col md:flex-row bg-cover bg-bottom" style="background-image: url({bg})">
-		<div class="h-5/6 w-5/6 rounded-t-full md:rounded-t-3xl md:rounded-b-3xl backdrop-saturate-200 bg-white/50 flex flex-col md:flex-row p-2">
+	<div
+		class="scroll-area flex-col md:flex-row bg-cover bg-bottom"
+		style="background-image: url({bg})"
+	>
+		<div
+			class="h-5/6 w-5/6 rounded-t-full md:rounded-t-3xl md:rounded-b-3xl backdrop-saturate-200 bg-white/50 flex flex-col md:flex-row p-2"
+		>
 			<div class="h-fit md:h-full w-fit left-top z-10 flex justify-center items-center">
 				<img class="object-contain" src={weddingLogo} alt="" />
 			</div>
@@ -42,9 +41,9 @@
 			</div>
 		</div>
 	</div>
-	
+
 	<div class="scroll-area bg-white flex flex-row relative">
-		<KlurAndDao img1={bg4} img2={bg5} ></KlurAndDao>
+		<KlurAndDao img1={bg4} img2={bg5} />
 	</div>
 
 	<div class="scroll-area">
@@ -57,46 +56,30 @@
 		<div class="h-1/6 w-fulltext-center">
 			<p class="my-auto text-black font-kanit">Timeline</p>
 		</div>
-		<div class="flex flex-row h-5/6 w-full max-w-3xl mb-10">
-			<div class="w-1/2 mb-auto ml-auto">
-				<div class="flex relative mb-4">
-					<TimeLineItem image={item1} time="07.09" desc="พิธีตักบาตรเช้า" additionalClass="mx-auto"></TimeLineItem>
-					<div class="h-4 w-4 absolute -right-[10px] top-1/2 rounded-full bg-slate-600"></div>
-				</div>
-				<div class="flex relative">
-					<TimeLineItem image={item3} time="09.39" desc="พิธีหลั่งน้ำสังข์" additionalClass="mx-auto"></TimeLineItem>
-					<div class="h-4 w-4 absolute -right-[10px] top-1/2 rounded-full bg-slate-600"></div>
-				</div>
-			</div>
-			<div class="h-5/6 w-fit my-auto py-4 flex flex-col items-center">
-				<div class="w-1 h-full bg-black" />
-				
-			</div>
-			<div class="w-1/2 my-auto pt-24 mr-auto">
-				<div class="relative">
-					<TimeLineItem image={item2} time="08.09" desc="พิธีแห่ขันหมาก" additionalClass="mx-auto"></TimeLineItem>
-					<div class="h-4 w-4 absolute -left-[10px] top-1/2 rounded-full bg-slate-600"></div>
-				</div>
-				<div class="relative">
-					<TimeLineItem image={item4} time="11.00" desc="ร่วมรับประทานอาหาร" additionalClass="mx-auto"></TimeLineItem>
-					<div class="h-4 w-4 absolute -left-[10px] top-1/2 rounded-full bg-slate-600"></div>
-				</div>
-			</div>
-		</div>
+		<TimeLine></TimeLine>
 	</div>
 
 	<div class="scroll-area flex flex-col md:flex-row py-8">
 		<div class="left-top flex-col h-1/2 md:h-full w-full mx-auto">
-			<div class="text-base text-center w-fit text-black flex flex-col mx-auto my-4">
+			<!-- <div class="text-base text-center w-fit text-black flex flex-col mx-auto my-4">
 				<img class="h-12 w-12 mx-auto" src={locationLogo} />
 				<p>ณ 155-Camp หนองกลางนา</p>
 				<p>อำเภอเมือง จังหวัดราชบุรี</p>
-			</div>
+			</div> -->
+			<img class="h-8 w-8 mx-auto" src={locationLogo} />
 			<div class="h-3/4 md:h-1/2 w-4/5 mx-auto shadow-xl">
-					<Map />
-					<!-- <div class="w-24 bg-slate-400 text-base px-8 py-2 my-8 rounded-lg shadow-lg hover:scale-125 transition-all delay-100">
-						<a class="h-full w-full m-auto" target="_blank" href="https://www.google.com/maps/place/155-Camp+%E0%B8%AB%E0%B8%99%E0%B8%AD%E0%B8%87%E0%B8%81%E0%B8%A5%E0%B8%B2%E0%B8%87%E0%B8%99%E0%B8%B2+155+%E0%B8%95%E0%B8%B3%E0%B8%9A%E0%B8%A5+%E0%B8%AB%E0%B8%99%E0%B8%AD%E0%B8%87%E0%B8%81%E0%B8%A5%E0%B8%B2%E0%B8%87%E0%B8%99%E0%B8%B2+%E0%B9%80%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B8%87+%E0%B8%A3%E0%B8%B2%E0%B8%8A%E0%B8%9A%E0%B8%B8%E0%B8%A3%E0%B8%B5+70000/@13.6099959,99.7975365,17z/data=!4m6!3m5!1s0x30e3250d8f1e5909:0x54d6a4e0440e274e!8m2!3d13.6099959!4d99.7975365!16s%2Fg%2F11pb8c7ggm">Direction</a>
-					</div> -->
+				<Map />
+				<div class="text-base text-center w-fit text-black flex flex-col mx-auto my-4">
+					<div class="flex backdrop-blur-xl bg-white/50 rounded-3xl shadow-xl pr-4">
+						<div class="h-12 w-24 bg-slate-400 rounded-full p-2">
+							<a class="h-full w-full m-auto" target="_blank" href="https://www.google.com/maps/place/155-Camp+%E0%B8%AB%E0%B8%99%E0%B8%AD%E0%B8%87%E0%B8%81%E0%B8%A5%E0%B8%B2%E0%B8%87%E0%B8%99%E0%B8%B2+155+%E0%B8%95%E0%B8%B3%E0%B8%9A%E0%B8%A5+%E0%B8%AB%E0%B8%99%E0%B8%AD%E0%B8%87%E0%B8%81%E0%B8%A5%E0%B8%B2%E0%B8%87%E0%B8%99%E0%B8%B2+%E0%B9%80%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B8%87+%E0%B8%A3%E0%B8%B2%E0%B8%8A%E0%B8%9A%E0%B8%B8%E0%B8%A3%E0%B8%B5+70000/@13.6099959,99.7975365,17z/data=!4m6!3m5!1s0x30e3250d8f1e5909:0x54d6a4e0440e274e!8m2!3d13.6099959!4d99.7975365!16s%2Fg%2F11pb8c7ggm">Direction</a>
+						</div>
+						<div class="text-base">
+							<p>ณ 155-Camp หนองกลางนา</p>
+							<p>อำเภอเมือง จังหวัดราชบุรี</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="right-buttom text-black text-center w-screen md:w-3/4">
@@ -113,7 +96,6 @@
 </div>
 
 <style>
-
 	@font-face {
 		font-family: 'Stylish Calligraphy';
 		src: url('/fonts/Stylish Calligraphy Demo.ttf') format('truetype');
